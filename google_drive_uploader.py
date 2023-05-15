451943065e09d8542edb0f18afaf4f224c9d2f7c
kuma <kuma@augusta-lab.co.jp>
2023-05-15 11:21:34 +0900

    アップロード対象ファイル取得処理の関数化とファイル情報を管理するためのデータクラスを追加

--- google_drive_uploader.py
+++ google_drive_uploader.py
@@ -1,3 +1,4 @@
+from dataclasses import asdict, dataclass, field
 import glob
 import os
 
@@ -6,14 +7,33 @@ from pydrive2.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-SERVICE_ACCOUNT_PATH = "YOUR_SERCICE_ACCOUNT_JSON_FILE_PATH"
-LOCAL_FOLDER_PATH = "YOUR_LOCAL_FOLDER_PATH"
-GOOGLE_DRIVE_FOLDER_ID = "YOUR_GOOGLE_DRIVE_FOLDER_ID"
+SERVICE_ACCOUNT_PATH = "service_account.json"
+LOCAL_FOLDER_PATH = r"C:\Users\kuma\Downloads\test"
+GOOGLE_DRIVE_FOLDER_ID = "1gakGiYh_x6G34k_D9CgvBtKTl01B2Nt2"
+
+
+@dataclass
+class Parents:
+    id: str = field(init=False)
+
+    def __post_init__(self):
+        self.id = GOOGLE_DRIVE_FOLDER_ID
+
+
+@dataclass(frozen=True)
+class MetaData:
+    title: str
+    parents: list[Parents]
+
+
+@dataclass
+class UploadFileInfo:
+    path: str
+    metadata: MetaData
 
 
 def create_google_drive() -> GoogleDrive:
     """Google Driveインスタンスの生成"""
-    # Google Drive APIに認証する
     gauth = GoogleAuth()
     scope = ["https://www.googleapis.com/auth/drive"]
     gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_PATH, scope)
@@ -21,16 +41,38 @@ def create_google_drive() -> GoogleDrive:
     return GoogleDrive(gauth)
 
 
-def upload_to_google_drive(file_path: str) -> None:
+def get_upload_files() -> list[UploadFileInfo]:
+    """アップロード対象のファイル情報を取得する"""
+    upload_files = []
+
+    if not os.path.isdir(LOCAL_FOLDER_PATH):
+        raise FileNotFoundError(f"アップロード対象のローカルディレクトリが存在しません。LOCAL_FOLDER_PATH: {LOCAL_FOLDER_PATH}")
+
+    files = glob.glob(f"{LOCAL_FOLDER_PATH}\*.csv")
+    if not files:
+        raise FileNotFoundError(f"アップロード対象ディレクトリ内のファイルが0件でした。LOCAL_FOLDER_PATH: {LOCAL_FOLDER_PATH}")
+
+    for file in files:
+        upload_files.append(UploadFileInfo(file, MetaData(os.path.basename(file), [Parents()])))
+
+    return upload_files
+
+
+def upload_to_google_drive(file_info: UploadFileInfo) -> None:
     """指定ファイルをGoogle Driveにアップロードする"""
-    upload_file = drive.CreateFile({"title": os.path.basename(file_path), 'parents': [{'id': GOOGLE_DRIVE_FOLDER_ID}]})
-    upload_file.SetContentFile(file_path)
+    upload_file = drive.CreateFile(asdict(file_info.metadata))
+    upload_file.SetContentFile(file_info.path)
     upload_file.Upload()
 
-    print(f'{file_path}のアップロードが完了しました。')
+    print(f'{file_info.metadata.title} をアップロードしました。')
 
 
-drive = create_google_drive()
-files = glob.glob(f"{LOCAL_FOLDER_PATH}\*.csv")
-for file in files:
-    upload_to_google_drive(file)
+if __name__ == '__main__':
+    try:
+        drive = create_google_drive()
+        upload_files = get_upload_files()
+        for upload_file in upload_files:
+            upload_to_google_drive(upload_file)
+
+    except Exception as e:
+        print(e)
